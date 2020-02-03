3c0866ab0b234cfb00fd26616d3c82633f1cbd64
Jon Dunlap <jon@the364.com>
2020-02-03 12:39:29 -0500

    Add exception import

--- core/src/main/java/net/shadowfacts/discordchat/core/DiscordChat.java
+++ core/src/main/java/net/shadowfacts/discordchat/core/DiscordChat.java
@@ -5,6 +5,7 @@ import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
 import net.dv8tion.jda.core.entities.*;
 import net.dv8tion.jda.core.exceptions.ErrorResponseException;
+import net.dv8tion.jda.core.exceptions.InsufficientPermissionException;
 import net.dv8tion.jda.core.exceptions.RateLimitedException;
 import net.dv8tion.jda.core.requests.RestAction;
 import net.shadowfacts.discordchat.api.*;
